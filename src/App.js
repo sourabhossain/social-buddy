a8978b15f13065204cd486dca5d35e42bb9d10c3
Sourab Hossain <sourabhossain.fci@gmail.com>
2020-09-27 22:13:33 +0600

    ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
+
+// coded by - taib islam dipu, 6 sept 2020
+
 import React from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Home from './components/Home/Home';
+import NoMatch from './components/NoMatch/NoMatch'
+import Comment from './components/Comment/Comment';
+
 function App() {
+
   return (
-    <div>
-        <h1>Sourab Hossain</h1>
+
+    <div className="bodyStyle">
+
+      <Router>
+        <Switch>
+
+          <Route path="/home">
+            <Home />
+          </Route>
+
+          <Route exact path="/">
+            <Home />
+          </Route>
+
+          <Route path="/post/:id">
+            <Comment />
+          </Route>
+
+          <Route path="*">
+            <NoMatch />
+          </Route>
+
+        </Switch>
+      </Router>
     </div>
+
   );
 }
 
